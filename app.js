4417667f6a3d157454d5c79602d86fa2fddf4206
Gott50 <xxGott50xx@gmail.com>
2017-11-29 08:45:54 +0100

    fix caught Error at /event with req: [object Object]; res: [object Object] : TypeError: Converting circular structure to JSON
        2017-11-29T07:44:34.363836+00:00 app[web.1]:     at JSON.stringify (<anonymous>)
    
        2017-11-29T07:44:34.363836+00:00 app[web.1]:     at stringify (/app/node_modules/express/lib/response.js:1064:12)
    
        2017-11-29T07:44:34.363837+00:00 app[web.1]:     at ServerResponse.json (/app/node_modules/express/lib/response.js:243:14)
    
        2017-11-29T07:44:34.363838+00:00 app[web.1]:     at /app/app.js:131:25
    
        2017-11-29T07:44:34.363839+00:00 app[web.1]:     at Layer.handle [as handle_request] (/app/node_modules/express/lib/router/layer.js:95:5)
    
        2017-11-29T07:44:34.363839+00:00 app[web.1]:     at next (/app/node_modules/express/lib/router/route.js:131:13)
    
        2017-11-29T07:44:34.363840+00:00 app[web.1]:     at Route.dispatch (/app/node_modules/express/lib/router/route.js:112:3)
    
        2017-11-29T07:44:34.363841+00:00 app[web.1]:     at Layer.handle [as handle_request] (/app/node_modules/express/lib/router/layer.js:95:5)
    
        2017-11-29T07:44:34.363842+00:00 app[web.1]:     at /app/node_modules/express/lib/router/index.js:277:22
    
        2017-11-29T07:44:34.363842+00:00 app[web.1]:     at Function.process_params (/app/node_modules/express/lib/router/index.js:330:12)
    
        2017-11-29T07:44:34.363843+00:00 app[web.1]:     at next (/app/node_modules/express/lib/router/index.js:271:10)
    
        2017-11-29T07:44:34.363844+00:00 app[web.1]:     at serveStatic (/app/node_modules/serve-static/index.js:75:16)
    
        2017-11-29T07:44:34.363845+00:00 app[web.1]:     at Layer.handle [as handle_request] (/app/node_modules/express/lib/router/layer.js:95:5)
    
        2017-11-29T07:44:34.363845+00:00 app[web.1]:     at trim_prefix (/app/node_modules/express/lib/router/index.js:312:13)
    
        2017-11-29T07:44:34.363846+00:00 app[web.1]:     at /app/node_modules/express/lib/router/index.js:280:7
    
        2017-11-29T07:44:34.363847+00:00 app[web.1]:     at Function.process_params (/app/node_modules/express/lib/router/index.js:330:12)

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ app.post('/send', function (req, res) {
 app.post('/event', function (req, res) {
     try {
         console.log("/event", req.body);
-        res.status(200).json({req: req})
+        res.status(200).json({req: JSON.stringify(req)})
     } catch (err) {
         console.error("caught Error at /event with req: %s; res: %s :", req.body, res, err);
         res.status(500).json({error: err})
